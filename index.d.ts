db54d7ab687eab290853517d19855dff6b23477f
sucaiquan <chianquan@qq.com>
2019-12-27 10:28:32 +0800

    移除编译出的文件

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-import { RoutingControllersSwaggerOptions } from './app/middleware/routing-controllers-swagger';
-export declare function ParamsWithOpenAPI(): (object: object, methodName: string, index: number) => void;
-export declare function QueriesWithOpenAPI(): (object: object, methodName: string, index: number) => void;
-declare module 'egg' {
-    interface EggAppConfig {
-        routingControllersSwagger: RoutingControllersSwaggerOptions;
-    }
-}
