7d17b48576089b86808ff6078b0a81f20ce72d74
chianquan <chianquan@qq.com>
2024-12-26 10:53:57 +0800

    暂存

--- app/middleware/routing-controllers-swagger.ts
+++ app/middleware/routing-controllers-swagger.ts
@@ -1,18 +1,20 @@
-import assert = require('assert');
-import {defaultMetadataStorage} from 'class-transformer/storage';
-import {getFromContainer, MetadataStorage} from 'class-validator';
-import {validationMetadatasToSchemas} from 'class-validator-jsonschema';
-import ejs = require('ejs');
-import fs = require('fs');
-import mount = require('koa-mount');
-import KoaRouter = require('koa-router');
-import koaStatic = require('koa-static');
-import {InfoObject} from 'openapi3-ts';
-import {getMetadataArgsStorage, RoutingControllersOptions} from 'routing-controllers';
-import {routingControllersToSpec} from 'routing-controllers-openapi';
-import swaggerUIDist = require('swagger-ui-dist');
+import assert = require("assert");
+import { defaultMetadataStorage } from "class-transformer/cjs/storage";
+import { validationMetadatasToSchemas } from "class-validator-jsonschema";
+import ejs = require("ejs");
+import fs = require("fs");
+import mount = require("koa-mount");
+import KoaRouter = require("koa-router");
+import koaStatic = require("koa-static");
+import { InfoObject } from "openapi3-ts/oas31";
+import {
+  getMetadataArgsStorage,
+  RoutingControllersOptions,
+} from "routing-controllers";
+import { routingControllersToSpec } from "routing-controllers-openapi";
+import swaggerUIDist = require("swagger-ui-dist");
 // @ts-ignore
-import pkg = require('../../package.json');
+import pkg = require("../../package.json");
 
 export interface RoutingControllersSwaggerOptions {
   routingControllersOptions: RoutingControllersOptions;
@@ -24,12 +26,12 @@ export interface RoutingControllersSwaggerOptions {
 export default (options: RoutingControllersSwaggerOptions) => {
   const {
     routingControllersOptions,
-    mountPath = '/swagger',
+    mountPath = "/swagger",
     enable = true,
     additionalProperties = {
-      description: 'swagger文档',
-      title: 'swagger文档',
-      version: '0.0.1',
+      description: "swagger文档",
+      title: "swagger文档",
+      version: "0.0.1",
     },
   } = options;
   if (!enable) {
@@ -37,53 +39,57 @@ export default (options: RoutingControllersSwaggerOptions) => {
       return next();
     };
   }
-  assert(routingControllersOptions, `[${(pkg as any).name}] routingControllersOptions is required on config`);
+  assert(
+    routingControllersOptions,
+    `[${(pkg as any).name}] routingControllersOptions is required on config`
+  );
   assert(mountPath, `[${(pkg as any).name}] mountPath is required on config`);
 
   const router = new KoaRouter();
 
-  const template = fs.readFileSync(`${__dirname}/../view/swaggerIndex.ejs`).toString();
+  const template = fs
+    .readFileSync(`${__dirname}/../view/swaggerIndex.ejs`)
+    .toString();
   let swaggerIndexHtml;
-  router.get('/index.html', (ctx) => {
+  router.get("/index.html", (ctx) => {
     if (!swaggerIndexHtml) {
       swaggerIndexHtml = ejs.render(template, {
-        swaggerJsonField: './swagger.json',
+        swaggerJsonField: "./swagger.json",
       });
     }
     ctx.body = swaggerIndexHtml;
   });
-  router.get('/', (ctx) => {
+  router.get("/", (ctx) => {
     ctx.body = `<script>location.href=location.href[location.href.length-1]==='/'?location.href+"index.html":location.href+'/index.html'</script>`;
   });
 
   const createSpec = () => {
-
-    const metadatas = (getFromContainer(MetadataStorage)as any).validationMetadatas;
-    const schemas = validationMetadatasToSchemas(metadatas, {
-      refPointerPrefix: '#/components/schemas/',
+    const schemas = validationMetadatasToSchemas({
+      refPointerPrefix: "#/components/schemas/",
       classTransformerMetadataStorage: defaultMetadataStorage,
     });
     // Parse routing-controllers classes into OpenAPI spec:
     const storage = getMetadataArgsStorage();
     const spec = routingControllersToSpec(storage, routingControllersOptions, {
-      components: {schemas},
+      components: { schemas: schemas as any },
       info: additionalProperties,
     });
     return spec;
   };
   let specCache: any;
-  router.get('/swagger.json', (ctx) => {
+  router.get("/swagger.json", (ctx) => {
     if (!specCache) {
       specCache = createSpec();
-      const schemas = specCache.components && specCache.components.schemas || {};
+      const schemas =
+        (specCache.components && specCache.components.schemas) || {};
       if (!schemas.Object) {
         schemas.Object = {
-          type: 'object',
+          type: "object",
         };
       }
       if (!schemas.Array) {
         schemas.Array = {
-          type: 'array',
+          type: "array",
           items: {},
         };
       }
@@ -92,7 +98,7 @@ export default (options: RoutingControllersSwaggerOptions) => {
   });
 
   const pathToSwaggerUi = swaggerUIDist.absolutePath();
-  router.get('/(.*)', mount(mountPath, koaStatic(pathToSwaggerUi)));
+  router.get("/(.*)", mount(mountPath, koaStatic(pathToSwaggerUi)));
   if (mountPath) {
     const pRouter = new KoaRouter();
     pRouter.use(mountPath, router.routes());
--- index.ts
+++ index.ts
@@ -1,108 +1,21 @@
-import {Params, QueryParams} from 'routing-controllers';
+import { RoutingControllersSwaggerOptions } from "./app/middleware/routing-controllers-swagger";
 
-import {defaultMetadataStorage} from 'class-transformer/storage';
-import {
-  getFromContainer,
-  MetadataStorage,
-} from 'class-validator';
-import {validationMetadatasToSchemas} from 'class-validator-jsonschema';
-import _ = require('lodash');  // todo remove
-import {ParameterObject} from 'openapi3-ts';
-import {RoutingControllersSwaggerOptions} from './app/middleware/routing-controllers-swagger';
+export * from "class-validator-jsonschema";
+export * from "routing-controllers-openapi";
 
-export * from 'class-validator-jsonschema';
-export * from 'routing-controllers-openapi';
-
-import classValidatorJsonSchema = require('class-validator-jsonschema');
-import routingControllersOpenapi = require('routing-controllers-openapi');
+import classValidatorJsonSchema = require("class-validator-jsonschema");
+import routingControllersOpenapi = require("routing-controllers-openapi");
 
 // declare all the dependence decorator,so that IDE can recognize and use the auto import feature.
 export const JSONSchema = classValidatorJsonSchema.JSONSchema;
 export const OpenAPI = routingControllersOpenapi.OpenAPI;
 export const ResponseSchema = routingControllersOpenapi.ResponseSchema;
-export const applyOpenAPIDecorator = routingControllersOpenapi.applyOpenAPIDecorator;
+export const applyOpenAPIDecorator =
+  routingControllersOpenapi.applyOpenAPIDecorator;
 export type OpenAPIParam = routingControllersOpenapi.OpenAPIParam;
 
-// extends routing-controllers Params ,make description for params take effect.
-export function ParamsWithOpenAPI() {
-  return (object: object, methodName: string, index: number) => {
-    Params()(object, methodName, index);
-    const type = Reflect.getMetadata('design:paramtypes', object, methodName)[index];
-    const targetName = type.name;
-    OpenAPI((source) => {// todo tmp
-      const metadatas = (getFromContainer(MetadataStorage)as any).validationMetadatas.filter((validationMetadata) => {
-        return validationMetadata.target.name === targetName;
-      });
-
-      const schemas = validationMetadatasToSchemas(metadatas, {
-        refPointerPrefix: '#/components/schemas/',
-        classTransformerMetadataStorage: defaultMetadataStorage,
-      });
-      const schema = schemas[targetName];
-      if (!schema) {
-        return source;
-      }
-      source.parameters = _.chain(schema.properties)
-        .toPairs()
-        .map(([name, propertyInfo]) => ({
-          in: 'path',
-          name,
-          required: true,
-          schema: {
-            type: 'string',
-          },
-          description: propertyInfo.description || '',
-        } as ParameterObject))
-        .concat(source.parameters as ParameterObject[])
-        .reject(({description}) => description === undefined)
-        .value();
-      return source;
-    })(object, methodName, {} as PropertyDescriptor);
-  };
-}
-
-// extends routing-controllers QueryParams ,make description for QueryParams take effect.
-export function QueriesWithOpenAPI() {
-  return (object: object, methodName: string, index: number) => {
-    QueryParams()(object, methodName, index);
-    // const tmp = Reflect.getMetadata('design:paramtypes', object, methodName);
-    const type = Reflect.getMetadata('design:paramtypes', object, methodName)[index];
-    // console.log(tmp, type);
-    const targetName = type.name;
-    OpenAPI((source) => {
-      const metadatas = (getFromContainer(MetadataStorage)as any).validationMetadatas.filter((validationMetadata) => {
-        return validationMetadata.target.name === targetName;
-      });
-
-      const schemas = validationMetadatasToSchemas(metadatas, {
-        refPointerPrefix: '#/components/schemas/',
-        classTransformerMetadataStorage: defaultMetadataStorage,
-      });
-      const schema = schemas[targetName];
-      if (!schema) {
-        return source;
-      }
-      source.parameters = _.chain(schema.properties)
-        .toPairs()
-        .map(([name, propertyInfo]) => ({
-          in: 'query',
-          name,
-          required: false,
-          schema: {
-            type: propertyInfo.type,
-          },
-          description: propertyInfo.description || '',
-        } as ParameterObject))
-        .concat(source.parameters as ParameterObject[])
-        .reject(({description}) => description === undefined)
-        .value();
-      return source;
-    })(object, methodName, {} as PropertyDescriptor);
-  };
-}
-
 // @ts-ignore
-declare module 'egg' {
+declare module "egg" {
   export interface EggAppConfig {
     routingControllersSwagger: RoutingControllersSwaggerOptions;
   }
